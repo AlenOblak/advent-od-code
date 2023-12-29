213db36644319baa0fa609f7c0666344bf60826e
Alen Oblak <alen.oblak@in2.si>
2023-12-29 07:43:52 +0100

    Added day 21 solution (part 2)

--- 2023/21/solve.php
+++ 2023/21/solve.php
@@ -1,6 +1,6 @@
 <?php
 
-$lines = file('input_small.txt', FILE_IGNORE_NEW_LINES);
+$lines = file('input.txt', FILE_IGNORE_NEW_LINES);
 
 $rock = array();
 $map = array();
@@ -16,20 +16,42 @@ foreach ($lines as $y => $line) {
 $max_y = count($lines);
 $max_x = strlen($lines[0]);
 
-$map1 = array();
-$map2 = $map;
-
 // part 1
-$sum1 = 1;
-$sum2 = 0;
-for($i = 1; $i <= 64; $i++) {
-    $step = step();
-    if($i % 2 == 0) {
-        $sum1 += $step;
-        echo $i.' '.$step.' '.$sum1."\n";
-    } else {
-        $sum2 += $step;
-        echo $i.' '.$step.' '.$sum2."\n";
+echo num_after_steps(64)."\n";
+
+// part 2
+$steps = 26501365;
+$full_grids = intdiv($steps, $max_y);
+$remain_steps = $steps % $max_y;
+$num_full_grids = ($full_grids * $full_grids * 2) - $full_grids - $full_grids + 1;
+
+$area_odd = num_after_steps($max_y);
+$area_even = num_after_steps($max_y + 1);
+$area_odd_corner = $area_odd - num_after_steps(65);
+$area_even_corner = $area_even - num_after_steps(64);
+$result = (($full_grids + 1) * ($full_grids + 1) * $area_odd) + ($full_grids * $full_grids * $area_even) - (($full_grids + 1) * $area_odd_corner) + ($full_grids  * $area_even_corner);
+
+echo $result."\n";
+
+function num_after_steps($steps) {
+    global $map, $map1, $map2;
+
+    $map1 = array();
+    $map2 = $map;
+
+    $sum1 = 1;
+    $sum2 = 0;
+    for($i = 1; $i <= $steps; $i++) {
+        $step = step();
+        if($i % 2 == 0) {
+            $sum1 += $step;
+            if($i == $steps)
+                return $sum1;
+        } else {
+            $sum2 += $step;
+            if($i == $steps)
+                return $sum2;
+        }
     }
 }
 
@@ -40,15 +62,6 @@ function step() {
 
     foreach ($map2 as $p => $v) {
         $p = explode(':', $p);
-        if(!isset($rock[($p[0]-1) % $max_y][$p[1] % $max_x]) && !isset($map1[($p[0]-1).':'.$p[1]]))
-            $new_map[($p[0]-1).':'.$p[1]] = true;
-        if(!isset($rock[($p[0]+1) % $max_y][$p[1] % $max_x]) && !isset($map1[($p[0]+1).':'.$p[1]]))
-            $new_map[($p[0]+1).':'.$p[1]] = true;
-        if(!isset($rock[$p[0] % $max_y][($p[1]-1)  % $max_x]) && !isset($map1[$p[0].':'.($p[1]-1)]))
-            $new_map[$p[0].':'.($p[1]-1)] = true;
-        if(!isset($rock[$p[0] % $max_y][($p[1]+1)  % $max_x]) && !isset($map1[$p[0].':'.($p[1]+1)]))
-            $new_map[$p[0].':'.($p[1]+1)] = true;
-        /*
         if($p[0] > 0 && !isset($rock[$p[0]-1][$p[1]]) && !isset($map1[($p[0]-1).':'.$p[1]]))
             $new_map[($p[0]-1).':'.$p[1]] = true;
         if($p[0] < $max_y-1 && !isset($rock[$p[0]+1][$p[1]]) && !isset($map1[($p[0]+1).':'.$p[1]]))
@@ -57,11 +70,9 @@ function step() {
             $new_map[$p[0].':'.($p[1]-1)] = true;
         if($p[1] < $max_x-1 && !isset($rock[$p[0]][$p[1]+1]) && !isset($map1[$p[0].':'.($p[1]+1)]))
             $new_map[$p[0].':'.($p[1]+1)] = true;
-        */
     }
     $map1 = $map2;
     $map2 = $new_map;
 
     return count($new_map);
 }
-
